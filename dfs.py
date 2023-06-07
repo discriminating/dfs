5a4700fa508f43ba7c39ea65ffd49f55f41f4e41
I am <99683656+discriminating@users.noreply.github.com>
2023-06-07 14:01:29 +0100

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -1,143 +1,96 @@
-import discord, json, os
+import discord, asyncio, os
 from discord.ext import commands
-
 
 bot = commands.Bot(command_prefix='$', help_command=None, intents=discord.Intents.all())
 
 config = {
-    "collaboration": True
+    "collaboration": True # if False, only the server owner can create channels
 }
 
 @bot.event
 async def on_ready():
-    print(f'dfs online as {bot.user}')
+    print(f'dfs online as {bot.user}, please bear in mind dfs is only designed for one server.')
 
 @bot.command()
 async def setup(ctx):
-    for channel in ctx.guild.channels:
-        await channel.delete()
-    for category in ctx.guild.categories:
-        await category.delete()
+    await asyncio.gather(*[channel.delete() for channel in ctx.guild.channels])
+    await asyncio.gather(*[category.delete() for category in ctx.guild.categories])
 
-    terminal = await ctx.guild.create_text_channel("terminal")
-
-#TODO: make prettier
-def makefile(channel):
-    if channel.category:
-        category_name = channel.category.name
-
-        if os.path.isdir(category_name):
-            with open(f"{category_name}/{channel.name.replace('-', '.')}", "w") as f: 
-                f.write("")
-        else:
-            os.mkdir(category_name)
-            with open(f"{category_name}/{channel.name.replace('-', '.')}", "w") as f: 
-                f.write("")
-    else:
-        with open(f"{channel.name.replace('-', '.')}", "w") as f: 
-            f.write("")
+    await ctx.guild.create_text_channel("terminal")
 
 @bot.event
 async def on_guild_channel_create(channel):
-    if isinstance(channel, discord.VoiceChannel):
-        print("voice channel created"); return
+    if isinstance(channel, discord.VoiceChannel) or channel.name == "terminal": return
 
+    async for entry in channel.guild.audit_logs(limit=1, action=discord.AuditLogAction.channel_create):
+        if not config["collaboration"] and entry.user != channel.guild.owner: await channel.delete(); continue
+        
+        if isinstance(channel, discord.TextChannel):
+            if channel.category:
+                category_name = channel.category.name
+                open(f"{category_name}/{channel.name.replace('-', '.')}", "w").close()
+            else: open(f"{channel.name.replace('-', '.')}", "w").close()
 
-    if channel.name == "terminal": return
-    else: 
-        async for entry in channel.guild.audit_logs(limit=1, action=discord.AuditLogAction.channel_create):
-            
-            if not config["collaboration"] and entry.user != channel.guild.owner: 
-                await channel.delete()
-            else: 
-                if isinstance(channel, discord.TextChannel): makefile(channel)
-                elif isinstance(channel, discord.CategoryChannel): os.mkdir(channel.name)
+        elif isinstance(channel, discord.CategoryChannel):
+            os.mkdir(channel.name)
+
+# @bot.event
+# async def on_guild_channel_edit(before, after):
+# for some reason on_guild_channel_edit isn't.. called?
 
 @bot.event
 async def on_guild_channel_delete(channel):
-    if isinstance(channel, discord.VoiceChannel):
-        print("voice channel deleted"); return
+    if isinstance(channel, discord.VoiceChannel) or channel.name == "terminal": return
 
-    if channel.name == "terminal": return
-    else:
-        if isinstance(channel, discord.TextChannel): 
-            if channel.category:
-                category_name = channel.category.name
-                os.remove(f"{category_name}/{channel.name.replace('-', '.')}")
-            else:
-                os.remove(f"{channel.name.replace('-', '.')}")
-        elif isinstance(channel, discord.CategoryChannel): 
-            os.rmdir(channel.name)
+    if isinstance(channel, discord.TextChannel):
+        if channel.category:
+            category_name = channel.category.name
+            os.remove(f"{category_name}/{channel.name.replace('-', '.')}")
+        else: os.remove(f"{channel.name.replace('-', '.')}")
+    
+    elif isinstance(channel, discord.CategoryChannel):
+        os.rmdir(f"{channel.name}")
 
-# on message
+async def writeChannelHistory(message, filepath):
+    if message.author == bot.user: return
+
+    filepath = f""
+
+    if message.channel.category: filepath += f"{message.channel.category.name}/"
+    filepath += f"{message.channel.name.replace('-', '.')}"
+
+    with open(filepath, "w") as f:
+        channelHandle = bot.get_channel(message.channel.id)
+
+        messages = []
+        async for message in channelHandle.history(limit=None):
+            messages.append(message.content)
+        messages.reverse()
+
+        for message in messages:
+            # if message.content.startswith("```") and message.content.endswith("```"): message.content = message.content[3:-3]
+            f.write(f"{message}\n")
+
 @bot.event
 async def on_message(message):
-    # if the channel name is terminal
-    if message.channel.name == "terminal" and message.author != bot.user and not message.content.startswith("$"):
-        await message.channel.send("```" + os.popen(message.content).read() + "```")
+    if message.channel.name == "terminal" and message.author != bot.user and not message.content.startswith('$'):
+        out = os.popen(message.content).read()
+        await message.channel.send("```" + out + "```" if out != "" else "```no output```")
+    
     elif message.channel.name != "terminal" and message.author != bot.user and not message.content.startswith("$"):
-        # get all messages in the channel, loop through them, and write them to the file
-        if message.channel.category:
-            category_name = message.channel.category.name
-            with open(f"{category_name}/{message.channel.name.replace('-', '.')}", "w") as f: 
-                channel = bot.get_channel(message.channel.id)
-                
-                arr = []
-                async for message in channel.history(limit=None):
-                    arr.append(message.content)
-                # invert the array
-                arr.reverse()
-                # write the array to the file
-                for message in arr:
-                    f.write(f"{message}\n")
-        else:
-            with open(f"{message.channel.name.replace('-', '.')}", "w") as f: 
-                print(f"{message.channel.history(limit=None).flatten()[0].content}\n")
+        await writeChannelHistory(message)
 
-    elif message.content.startswith("$"):
-        await bot.process_commands(message)
+    elif message.content.startswith("$"): await bot.process_commands(message)
 
-# on message edit
 @bot.event
 async def on_message_edit(before, after):
-    if before.channel.name == "terminal" and before.author != bot.user and not before.content.startswith("$"):
-        await before.channel.send("```" + os.popen(before.content).read() + "```")
-    elif before.channel.name != "terminal" and before.author != bot.user and not before.content.startswith("$"):
-        # get all messages in the channel after the edited message, loop through them, and write them to the file
-        if before.channel.category:
-            category_name = before.channel.category.name
-            with open(f"{category_name}/{before.channel.name.replace('-', '.')}", "w") as f: 
-                channel = bot.get_channel(before.channel.id)
-                
-                arr = []
-                async for message in channel.history(limit=None):
-                    arr.append(message.content)
-                # invert the array
-                arr.reverse()
-                # write the array to the file
-                for message in arr:
-                    f.write(f"{message}\n")
+    if after.channel.name != "terminal" and after.author != bot.user and not after.content.startswith("$"):
+        await writeChannelHistory(after)
 
-# on message delete
 @bot.event
 async def on_message_delete(message):
     if message.channel.name != "terminal" and message.author != bot.user and not message.content.startswith("$"):
-        # get all messages in the channel, loop through them, and write them to the file
-        if message.channel.category:
-            category_name = message.channel.category.name
-            with open(f"{category_name}/{message.channel.name.replace('-', '.')}", "w") as f: 
-                channel = bot.get_channel(message.channel.id)
-                
-                arr = []
-                async for message in channel.history(limit=None):
-                    arr.append(message.content)
-                # invert the array
-                arr.reverse()
-                # write the array to the file
-                for message in arr:
-                    f.write(f"{message}\n")
-
-
+        await writeChannelHistory(message)
 
 with open("token.secret", "r") as f:
     token = f.read()
